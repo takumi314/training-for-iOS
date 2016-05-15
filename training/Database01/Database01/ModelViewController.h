45074271619a29bc1e6fe35a815a6a6af67ad00e
takumi314 <isospin_symmetry@me.com>
2016-05-15 14:18:17 +0900

    Create two new classes

---
+++ training/Database01/Database01/ModelViewController.h
@@ -0,0 +1,15 @@
+//
+//  ModelViewController.h
+//  Database01
+//
+//  Created by NishiokaKohei on 2016/05/15.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ModelViewController : UIViewController
+
+- (
+
+@end
