71cb196b4f02f578b8345a792b994bd8856af101
takumi314 <isospin_symmetry@me.com>
2016-05-18 00:34:52 +0900

    Create Model.h and Model.m in Model as new directory.

---
+++ training/TableView01/TableView01/Model.h
@@ -0,0 +1,15 @@
+//
+//  Model.h
+//  TableView01
+//
+//  Created by NishiokaKohei on 2016/05/18.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Model : NSObject
+
+
+
+@end
---
+++ training/TableView01/TableView01/Model.m
@@ -0,0 +1,13 @@
+//
+//  Model.m
+//  TableView01
+//
+//  Created by NishiokaKohei on 2016/05/18.
+//  Copyright © 2016年 Kohei. All rights reserved.
+//
+
+#import "Model.h"
+
+@implementation Model
+
+@end
