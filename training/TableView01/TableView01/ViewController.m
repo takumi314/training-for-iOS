69d21a15d697ef98c266660923fee27cbc282960
takumi314 <isospin_symmetry@me.com>
2016-05-18 00:25:05 +0900

    Controller.m# Add three methods for TableView.

--- training/TableView01/TableView01/ViewController.m
+++ training/TableView01/TableView01/ViewController.m
@@ -8,7 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource,UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -23,5 +24,44 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+
+#pragma mark - UITableViewDataSource
+
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+
+#pragma mark - UITableViewDelegate
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *_identifier =@"cell";
+    UITableViewCell *_cell = [tableView dequeueReusableCellWithIdentifier: _identifier];
+    
+    if (!_cell) {
+        _cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleValue1 reuseIdentifier: _identifier];
+    }
+    
+    
+    
+    _cell.textLabel.text = [NSString stringWithFormat:@"Section#%02d Row#%02d", (int)indexPath.section, (int)indexPath.row];
+    
+    return _cell;
+}
+
+
+
+
 
 @end
